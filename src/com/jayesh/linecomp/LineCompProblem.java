d8a7eef878e94ad654cdbf7b9a791e84d632bc6e
SonarJayesh <jayeshsonar2824@gmail.com>
2024-02-26 10:41:12 +0530

    [ADDED]: JAYESH This Code Show The Welcome to Line Comarison Problem Message

---
+++ src/com/jayesh/linecomp/LineCompProblem.java
@@ -0,0 +1,11 @@
+package com.jayesh.linecomp;
+
+public class LineCompProblem {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program ");
+
+	}
+
+}
