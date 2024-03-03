dbc8de282179d8bad0fecef081d163ce9e567ce1
SonarJayesh <jayeshsonar2824@gmail.com>
2024-03-03 13:53:48 +0530

    [MODIFIED]: JAYESH UC-2 Are completed Check Equality of two Lines

--- src/com/jayesh/linecomp/LineCompProblem.java
+++ src/com/jayesh/linecomp/LineCompProblem.java
@@ -1,30 +1,51 @@
 package com.jayesh.linecomp;
 
-import java.util.Scanner;
-
 public class LineCompProblem {
-
+		
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	String line;
+	
+	void setPoints(int x1, int y1, int x2, int y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
+	
+	double length() {
+		
+			return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));	
+	}
+	
+	public void checkEquality(LineCompProblem linecompproblem2)
+	{
+		
+		if(this.length() == linecompproblem2.length()) {
+		
+			System.out.println("Both Line are Same");
+		
+		}
+		else {
+			
+			System.out.println("Line are Not Same");
+		
+		}	
+	}
+	
 	public static void main(String[] args) {
 		
-		int x1;
-		int x2;
-		int y1 = 0;
-		int y2;
-		double length;
-		
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("Welcome to Line Comparison Computation Program ");
 		
-		System.out.println("Enter the X1,Y1 and X2,Y2 points for the Line:");
+		LineCompProblem linecompproblem1 = new LineCompProblem();
+		linecompproblem1.setPoints(20, 50, 10, 20);
 		
-		x1 = scanner.nextInt();
-		y2 = scanner.nextInt();
-		x2 = scanner.nextInt();
-		y2 = scanner.nextInt();
+		LineCompProblem linecompproblem2 = new LineCompProblem();
+		linecompproblem2.setPoints(20, 50, 10, 20);
 		
-		length = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		System.out.println("Length of Line = "+length);
-		
+		linecompproblem1.checkEquality(linecompproblem2);
+	
 	}
-
 }
